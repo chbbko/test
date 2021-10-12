05f2d496a0684b7213c56d1e077425a51ddd8f31
chbbko <92324492+chbbko@users.noreply.github.com>
2021-10-12 17:18:15 +0700

    Add files via upload

--- entities/appointment.go
+++ entities/appointment.go
@@ -1,20 +1,26 @@
 package entities
 
-import "time"
+import (
+	"time"
+)
 
 type AppointmentList struct {
 	PatiencePinCode string
 	DoctorID string
 	Phone string
 	Ref string
-	Start *time.Time
-	End *time.Time
+	Start time.Time
+	End time.Time
+	ID uint
 }
 
 type AppointmentCreate struct {
 	PatiencePinCode string
 	DoctorID string
 	Ref string
+	Phone string
+	Start time.Time
+	End time.Time
 }
 
 type AppointmentDelete struct {
@@ -29,4 +35,18 @@ type AppointmentListParams struct {
 type Doctor struct {
 	DoctorID string
 	Name string
+}
+
+type ScheduleParams struct {
+	DoctorID   string
+	Begin *int64
+	End *int64
+}
+
+type ScheduleList struct {
+	ID uint
+	Ref string
+	DoctorID string
+	Start *time.Time
+	End  *time.Time
 }
