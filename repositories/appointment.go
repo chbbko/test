d5363b1982623b7ec3ffb0297f2436d4f64af2bb
chbbko <92324492+chbbko@users.noreply.github.com>
2021-10-12 17:19:42 +0700

    Add files via upload

--- repositories/appointment.go
+++ repositories/appointment.go
@@ -6,4 +6,5 @@ type AppointmentRepository interface {
 	ListAppointment(entities.AppointmentListParams) ([]entities.AppointmentList, error)
 	Create(entities.AppointmentCreate) error
 	Cancel(entities.AppointmentDelete) error
+	ListSchedule(params entities.ScheduleParams) ([]entities.ScheduleList, error)
 }
